71f65de1158f2f9311eaa02feecfc017b439ada5
Kudi User <kudiuser@Kudis-MacBook-Pro.local>
2023-11-30 22:37:52 +0100

    Services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('auth')
+export class UserController {
+    constructor(private userService: UserService){
+        this.userService = userService
+    }
+
+    @Post("/signup")
+    createUser(@Body() body: CreateUserDto) {
+        return this.userService.create(body)
+    }
+
+    @Get("/users")
+    fetchUsers(@Query("email") email: string){
+        return this.userService.fetchAll(email)
+    }
+
+    @Get("/users/:id")
+    async fetchUser(@Param("id") id: number){
+        const user = await this.userService.fetchOne(id)
+
+        if(!user){
+            throw new NotFoundException(`User not found`)
+        }
+
+        return user
+    }
+
+    @Patch("/users/:id")
+    async updateUser(@Param("id") id: number, @Body() body: Partial<User>){
+        const user = await this.fetchUser(id)
+        return this.userService.update(user, body)
+    }
+
+    @Delete("users/:id")
+    async deleteUser(@Param("id") id: number){
+        const user = await this.fetchUser(id)
+        return this.userService.remove(user)
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  create(body: CreateUserDto) {
+    const user = this.userRepository.create(body);
+    return this.userRepository.save(user);
+  }
+
+  fetchAll(email: string) {
+    return this.userRepository.find({
+      where: {
+        email,
+      },
+    });
+  }
+
+  fetchOne(id: number) {
+    return this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  update(user: User, attrs: Partial<User>){
+      Object.assign(user, attrs)
+      return this.userRepository.save(user)
+  }
+
+  remove(user: User){
+    return this.userRepository.remove(user)
+}
+}
