7c18cf37d28d93b553dc345aa294bf0434f8a56e
Kudi User <kudiuser@Kudis-MacBook-Pro.local>
2023-11-30 20:31:11 +0100

    first commit

---
+++ src/report/report.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportController } from './report.controller';
+import { Report } from './report.entity';
+import { ReportService } from './report.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Report])],
+  controllers: [ReportController],
+  providers: [ReportService]
+})
+export class ReportModule {}
