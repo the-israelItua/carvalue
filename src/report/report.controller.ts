7572d3903c01af1455c04bac0efc63286cffa5b3
Kudi User <kudiuser@Kudis-MacBook-Pro.local>
2023-12-11 15:47:39 +0100

    Update approval

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/user/decorators/current-user.decorator';
 import { User } from 'src/user/user.entity';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
+import { Report } from './report.entity';
 import { ReportService } from './report.service';
 
 @Controller('report')
@@ -15,5 +17,10 @@ export class ReportController {
     @Serialize(ReportDto)
     createReport(@Body() body: CreateReportDto, @CurrentUser() user: User){
         return this.reportService.create(body, user)
+    }
+
+    @Patch("/:id")
+    approveReport(@Param() id: number, @Body() body: ApproveReportDto ){
+        return  this.reportService.updateApproval(id, body.approved)
     }
 }
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { report } from 'process';
 import { User } from 'src/user/user.entity';
@@ -8,10 +8,23 @@ import { Report } from './report.entity';
 
 @Injectable()
 export class ReportService {
-    constructor( @InjectRepository(Report) private reportRepository: Repository<Report> ){}
-    create(body: CreateReportDto, user: User){
-        const newReport = this.reportRepository.create(body)
-        newReport.user = user
-        return this.reportRepository.save(newReport)
+  constructor(
+    @InjectRepository(Report) private reportRepository: Repository<Report>,
+  ) {}
+  create(body: CreateReportDto, user: User) {
+    const newReport = this.reportRepository.create(body);
+    newReport.user = user;
+    return this.reportRepository.save(newReport);
+  }
+
+  async updateApproval(id: number, approved: boolean) {
+    const report = await this.reportRepository.findOneBy({ id });
+
+    if (!report) {
+      throw new NotFoundException('Report not found');
     }
+
+    report.approved = approved;
+    return this.reportRepository.save(report);
+  }
 }
